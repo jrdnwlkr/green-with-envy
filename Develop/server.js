f1c5530ab878e39c73b1e3076f2acb37676d2f8d
jrdnwlkr <walker.jordan.m@gmail.com>
2023-06-27 10:13:44 -0500

    env file created

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
